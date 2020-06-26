5d34e9685e38f3e5581deb09b12b607c1d2e7d50
Alena <alenaryzhko1@gmail.com>
2020-06-25 20:29:43 -0400

    changes added

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 def browser_init(context):
@@ -9,6 +10,7 @@ def browser_init(context):
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
+    context.driver.wait = WebDriverWait(context.driver, 15)
 
 
 def before_scenario(context, scenario):
@@ -28,3 +30,14 @@ def after_step(context, step):
 def after_scenario(context, feature):
     context.driver.delete_all_cookies()
     context.driver.quit()
+
+
+
+
+
+
+
+
+    # context.driver.maximize_window()
+    context.driver.implicitly_wait(10)
+    context.driver.wait = WebDriverWait(context.driver, 15)
