81b77551ae58760db950039e466e1e30389a31db
Alena <alenaryzhko1@gmail.com>
2020-10-05 12:49:19 -0400

    file was modified

--- bdd/features/steps/product_search_duckduckgo.py
+++ bdd/features/steps/product_search_duckduckgo.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 
 URL = 'https://www.duckduckgo.com'
-PHRASE = 'panda'
 SEARCH_FIELD = (By.ID, "search_form_input_homepage")
 SEARCH_BUTTON = (By.ID, "search_button_homepage")
 
@@ -12,16 +11,16 @@ def open_duckuckgohome_page(context):
     context.driver.get(URL)
 
 
-@when('the user searches for "panda"')
-def search_for_world(context):
+@when('the user searches for "{search_phrase}"')
+def search_for_world(context, search_phrase):
     search_input = context.driver.find_element(*SEARCH_FIELD)
-    search_input.send_keys(PHRASE)
+    search_input.send_keys(search_phrase)
     search_button = context.driver.find_element(*SEARCH_BUTTON)
     search_button.click()
 
 
-@then('result are shown for "panda"')
-def verify_result_appears_on_the_results_page(context):
+@then('search results for "{search_phrase}" should appear')
+def verify_result_appears_on_the_results_page(context, search_phrase):
     link_divs = context.driver.find_elements_by_css_selector('#links > div')
     assert len(link_divs) > 0
 
@@ -30,10 +29,10 @@ def verify_result_appears_on_the_results_page(context):
     assert len(link_divs) > 0
 
 # Verify that at least one search result contains the search phrase
-    xpath = f"//div[@id='links']//*[contains(text(), '{PHRASE}')]"
+    xpath = f"//div[@id='links']//*[contains(text(), '{search_phrase}')]"
     phrase_results = context.driver.find_elements_by_xpath(xpath)
     assert len(phrase_results) > 0
 
 # Verify that the search phrase is the same
     search_input = context.driver.find_element_by_id('search_form_input')
-    assert search_input.get_attribute('value') == PHRASE
+    assert search_input.get_attribute('value') == search_phrase
--- bdd/features/tests/product_search_duckduckgo.feature
+++ bdd/features/tests/product_search_duckduckgo.feature
@@ -5,5 +5,6 @@ Feature: DuckuckGo Web Browsing
 
   Scenario: Basic DuckuckGo Search
     Given the DuckuckGohome page is displayed
-    When the user searches for "panda"
-    Then result are shown for "panda"
+    When the user searches for "gift"
+    Then search results for "gift" should appear
+
