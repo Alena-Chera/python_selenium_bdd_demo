520931219a1490960519b633c30e7b50bed2a1a3
Alena <alenaryzhko1@gmail.com>
2020-10-05 19:06:18 -0400

    rename file

---
+++ simple_way_of_test_automation/search/product_search_duckduckgo_1.py
@@ -0,0 +1,19 @@
+"""
+Web test cases for DuckDuckGo search functionality
+DuckDuckGo is a search engine that doesn’t track user data.
+Users can enter search phrases and get links to matching websites.
+
+Test procedure:
+1.	Navigate to the DuckDuckGo home page
+2.	Enter the search phrase
+3.	Verify that:
+  a. Results appear on the results page
+  b. The search phrase appears in the search bar
+  c. At least one search result contains the search phrase
+It’s basic, but it covers typical searching behavior end-to-end.
+
+
+Feature: Search using DuckDuckGo
+Scenario: Basic DuckuckGo Search
+
+"""
