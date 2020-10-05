6357aa483e2271ee4ef9ce3dafe7702edd5c5c06
Alena <alenaryzhko1@gmail.com>
2020-10-05 18:31:18 -0400

    changed assert

--- simple_way_of_test_automation/search/product_search_duckduckgo.py
+++ simple_way_of_test_automation/search/product_search_duckduckgo.py
@@ -12,8 +12,13 @@ Test procedure:
   c. At least one search result contains the search phrase
 It’s basic, but it covers typical searching behavior end-to-end.
 
+
+Feature: Search using DuckDuckGo
+Scenario: Basic DuckuckGo Search
+
 """
 from selenium import webdriver
+
 
 # Initialize the ChromeDriver instance
 driver = webdriver.Chrome()
@@ -22,14 +27,22 @@ driver = webdriver.Chrome()
 # (Wait implicitly for elements to be ready before attempting interactions)
 driver.implicitly_wait(10)
 
+"""
+1
+Given the DuckuckGohome page is displayed
+"""
 # Set up some test case data
 URL = 'https://www.duckduckgo.com'
-PHRASE = 'panda'
+PHRASE = 'gift'
 
 # Navigate to the DuckDuckGo home page
 # open the url
 driver.get(URL)
 
+"""
+2
+When the user searches for "gift"
+"""
 # Find the search input element
 # In the DOM, it has an 'id' attribute of 'search_form_input_homepage'
 search_input = driver.find_element_by_id('search_form_input_homepage')
@@ -42,18 +55,32 @@ search_input.send_keys(PHRASE)
 search_button = driver.find_element_by_id("search_button_homepage")
 search_button.click()
 
+"""
+3
+Then search results for "gift" should appear
+"""
 # Verify that results appear on the results page
 link_divs = driver.find_elements_by_css_selector('#links > div')
 assert len(link_divs) > 0
 
+"""
+4
+The search phrase appears in the search bar
+"""
+# Verify that the search phrase is the same
+search_input = driver.find_element_by_css_selector('input#search_form_input')
+assert search_input.get_attribute('value') == PHRASE
+
+
+"""
+5
+And at least one search result contains the search phrase
+"""
 # Verify that at least one search result contains the search phrase
 xpath = f"//div[@id='links']//*[contains(text(), '{PHRASE}')]"
 phrase_results = driver.find_elements_by_xpath(xpath)
 assert len(phrase_results) > 0
 
-# Verify that the search phrase is the same
-search_input = driver.find_element_by_id('search_form_input')
-assert search_input.get_attribute('value') == PHRASE
 
 # Quit the WebDriver instance for the cleanup
 driver.quit()
