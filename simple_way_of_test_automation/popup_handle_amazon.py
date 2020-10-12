5bcf75fd7b174f6a2042651397dd0c4bb88f0a93
Alena <alenaryzhko1@gmail.com>
2020-10-12 14:19:15 -0400

    test added

--- simple_way_of_test_automation/popup_handle_amazon.py
+++ simple_way_of_test_automation/popup_handle_amazon.py
@@ -1,49 +1,53 @@
 """
-Web test cases for Amazon add product to cart functionality
-Users can enter search product and add product to cart
+           Python webdriver to handle pop up browser windows which is not an alert
+
+Web test cases for Amazon 'add to cart' functionality
+Users can enter search product and add product to cart, close pop up if it will appear
 
 Test procedure:
-1.	Navigate to the Amazon home page
-2.	Enter the search product
-3.	Select first product from results page
-4. When product page is open edd product to cart. Close pop up if it appears.
+1. Navigate to the Amazon home page
+2. Enter the search product
+3. Select first product from results page
+4. When product page will open edd product to cart. Close pop up if it will appear
 
-Feature: Search product on Amazon
-Scenario: Basic Amazon Search
+Feature: Add to cart
+Scenario: Amazon add to cart product, close pop up
 
 """
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Initialize the ChromeDriver instance
-from selenium.webdriver.common.keys import Keys
-
 driver = webdriver.Chrome()
 
 # Maximize window
-# We try to stay our test more stable - we open the window for full size
-# to make sure that user actually works with a full screen
 driver.maximize_window()
 
-# Make its calls wait up to 10 seconds for elements to appear
-# (Wait implicitly for elements to be ready before attempting interactions)
+# Wait implicitly for elements to be ready before attempting interactions
 driver.implicitly_wait(10)
 
 # Given the Amazon page is displayed
 driver.get('https://www.amazon.com/')
 
-# When the user searches for "tablet"
+# Search for "tablet", click on first matching result
 search_keyword = 'tablet'
-driver.find_element(By.ID, "twotabsearchtextbox").send_keys(search_keyword + Keys.ENTER)
-# OR
-# driver.find_element(By.ID,"twotabsearchtextbox").send_keys("tablet")
-# driver.find_element(By.NAME, "site-search").submit()
-# driver.find_element(By.ID,"twotabsearchtextbox").send_keys("tablet")
-# driver.find_element(By.CSS_SELECTOR, "input.nav-input[value='Go']").click()
+driver.find_element(By.ID,"twotabsearchtextbox").send_keys(search_keyword)
+driver.find_element(By.CSS_SELECTOR, "input.nav-input[value='Go']").click()
 
-# Then results page for "tablet" opened
-search_word_on_results_page = driver.find_element(By.CSS_SELECTOR, "span.a-color-state.a-text-bold").text
-assert search_keyword in search_word_on_results_page
+first_product = driver.find_elements(By.CSS_SELECTOR, "a.a-link-normal.s-no-outline")[0]
+first_product.click()
+
+# Click add to cart button
+wait = WebDriverWait(driver, 5)
+wait.until(expected_conditions.presence_of_element_located((By.ID, "add-to-cart-button"))).click()
+
+# Close popup if it appears
+if driver.find_elements(By.ID, 'a-popover-1') and driver.find_element(By.ID, 'a-popover-1').is_displayed():
+    driver.find_element_by_css_selector('#a-popover-1 .a-button-close').click()
 
 # Quit the WebDriver instance for the cleanup
 driver.quit()
+
+
