28f7b9b097396a43a0b79d5bc61f76c58f14771f
Alena <alenaryzhko1@gmail.com>
2020-10-12 14:09:58 -0400

    file modified

--- simple_way_of_test_automation/unit_tests/popup_handle_amazon.py
+++ simple_way_of_test_automation/unit_tests/popup_handle_amazon.py
@@ -20,10 +20,11 @@ class AmazonBestSellerExample(unittest.TestCase):
         self.driver.quit()
 
     def test_find_tablet(self):
-        search_keyword = 'tablet'
-
         driver = self.driver
         wait = WebDriverWait(driver, 5)
+
+        # Search for "tablet"
+        search_keyword = 'tablet'
         driver.find_element(By.ID, "twotabsearchtextbox").send_keys(search_keyword + Keys.ENTER)
         # OR
         # driver.find_element(By.ID,"twotabsearchtextbox").send_keys("tablet")
@@ -31,12 +32,14 @@ class AmazonBestSellerExample(unittest.TestCase):
         # driver.find_element(By.ID,"twotabsearchtextbox").send_keys("tablet")
         # driver.find_element(By.CSS_SELECTOR, "input.nav-input[value='Go']").click()
 
+        # Click on first matching result
         first_product = driver.find_elements(By.CSS_SELECTOR, "a.a-link-normal.s-no-outline")[0]
         first_product.click()
 
+        # Click add to cart button
         wait.until(expected_conditions.presence_of_element_located((By.ID, "add-to-cart-button"))).click()
 
-        # close popup if it appears
+        # Close popup if it appears
         if driver.find_elements(By.ID, 'a-popover-1') and driver.find_element(By.ID, 'a-popover-1').is_displayed():
             driver.find_element_by_css_selector('#a-popover-1 .a-button-close').click()
 
